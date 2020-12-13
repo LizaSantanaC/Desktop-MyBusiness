680c7eac628e864b0d6588ac846d77523af0c308
LizaSantanaC <47309634+LizaSantanaC@users.noreply.github.com>
2020-12-12 23:07:37 -0600

    Add files via upload
    VARIABLES DE INSTANCIA DE LA ENTIDAD PROVEEDOR

---
+++ PROYECTO/MyBusiness/src/main/java/vistas/proveedor/VariablesProveedores.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista.proveedor;
+
+import controlador.ProveedoreDAO;
+import modelo.Proveedor;
+import recursos.Recursos;
+
+/**
+ *
+ * @author liza
+ */
+public interface VariablesProveedores {
+    ProveedoreDAO pDAO = new ProveedoreDAO();
+}
